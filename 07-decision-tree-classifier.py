e7bec2a46f46b7f8d94d6dd42e9f6e63e180da73
fadi quader <fadi.quader@gmail.com>
2025-02-25 22:48:00 +0800

    add dtc and rfc

---
+++ 07-decision-tree-classifier.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+dataset = pd.read_csv('./datasets/wine.csv')
+
+features = dataset.drop(['Wine'], axis=1)
+labels = dataset['Wine']
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.25)
+
+# Training the model
+classifier = DecisionTreeClassifier()
+classifier.fit(features_train, labels_train)
+
+# Evaluate the model
+pred = classifier.predict(features_test)
+
+accuracy = accuracy_score(labels_test, pred)
+print(f"Accuracy: {accuracy}")
