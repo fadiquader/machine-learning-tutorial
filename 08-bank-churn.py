2c4220d293eb5947328175a5292069b48a1e83dc
fadi quader <fadi.quader@gmail.com>
2025-03-16 14:51:33 +0800

    add bank churn

---
+++ 08-bank-churn.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+dataset = pd.read_csv('./datasets/Churn_Modelling.csv')
+
+features = dataset.drop(['Outcome'], axis=1)
+labels = dataset['Outcome']
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.25)
+
+classifier = SVC()
+classifier.fit(features_train, labels_train)
+
+pred = classifier.predict(features_test)
+
+accuracy = accuracy_score(labels_test, pred)
+print(f"Accuracy: {accuracy}")
